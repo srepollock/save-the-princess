b87fb0cd41650c71e1d73dbe11c24e8c52179345
Spencer Pollock <srepollock@gmail.com>
2019-09-04 19:25:34 -0700

    feat(renderer): 2 Scenes down, more to come
    2 scenes are now working with the engine. Going to get zones 1.1,  1.2, 1.3 then release an alpha
    version. This will include scoring in the gaming as well.

--- README.md
+++ README.md
@@ -1,25 +1,11 @@
-# TypeScript Bootstrap
+# Divine Engine (Electron Latest build)
 
-A TypeScript bootstrap project: to start your project out without hassle. This project aims to give you the best tools you need to start your project on the right foot. When forking this project to start out your own unique idea, take some time to fill out the information here in this README. Remember, just like this README, your README.md file is the first contact to the end user. Most people don't read any more documentation (although I'm sure you'll have lots!) and really they should be able to get started with no more information than from here. Give them everything they need to get started on their own wonderful project.  
-The following is simply a template of information to give. It is not final, it is not correct, it is simply a guidline of best practices.
-
-Now remove this part and fill out the rest with your unique project information. Everything below can be removed, but some can be updated with your project specific information.
-*Remember to give credit where it is due.*
-
-> Created by [Spencer Pollock](@srepollock)
-> [Contact me](http://spollock.ca)
-
----
-
-# Project Title
-
-[A short project description goes here. Up to a paragraph but no more.]
 
 [![TypeScript](https://badges.frapsoft.com/typescript/version/typescript-next.svg?v=101)](https://github.com/ellerbrock/typescript-badges/)
 [![Open Source Love](https://badges.frapsoft.com/os/v2/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 
-Master: [![Build Status](https://travis-ci.org/srepollock/ts-bootstrap.svg?branch=master)](https://travis-ci.org/srepollock/ts-bootstrap) [![Coverage Status](https://coveralls.io/repos/github/srepollock/ts-bootstrap/badge.svg?branch=master)](https://coveralls.io/github/srepollock/ts-bootstrap?branch=master)
+Master: [![Build Status](https://travis-ci.org/srepollock/de-electron-latest.svg?branch=master)](https://travis-ci.org/srepollock/de-electron-latest) [![Coverage Status](https://coveralls.io/repos/github/srepollock/de-electron-latest/badge.svg?branch=master)](https://coveralls.io/github/srepollock/de-electron-latest?branch=master)
 
 ## Getting Started
 
@@ -40,44 +26,12 @@ A step by step series of examples that tell you how to get a development env run
 Say what the step will be
 
 ```
-Give the example
+npm start
 ```
-
-And repeat
-
-```
-until finished
-```
-
-End with an example of getting some data out of the system or using it for a little demo
-
-## Running the tests
-
-Explain how to run the automated tests for this system
-
-### Break down into end to end tests
-
-Explain what these tests test and why
-
-```
-Give an example
-```
-
-### And coding style tests
-
-Explain what these tests test and why
-
-```
-Give an example
-```
-
-## Deployment
-
-Add additional notes about how to deploy this on a live system
 
 ## Built With
 
-* [NodeJS](http://www.nodejs.com)
+- [Electron](https://electron.org)
 
 ## Contributing
 
@@ -89,7 +43,8 @@ We use [SemVer](http://semver.org/) for versioning. For the versions available, 
 
 ## Authors
 
-* **YOU** - [your profile](https://github.com/yourprofile)
+> Created by [Spencer Pollock](@srepollock)
+> [Contact me](http://spollock.ca)
 
 See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
 
@@ -104,6 +59,5 @@ This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md
 * etc
 * **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
 
-
-> Created by [YOU!] (@[your-github-username]). 
-> [Contact me](someone@example.com)
+Now remove this part and fill out the rest with your unique project information. Everything below can be removed, but some can be updated with your project specific information.
+*Remember to give credit where it is due.*
---
+++ src/renderer.ts
@@ -0,0 +1,303 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+import * as divine from "divine-engine";
+window.onload = () => {
+    process.env.NODE_DEBUG = "true";
+    divine.Engine.start({
+        assets: {
+            "materials": [
+                {
+                    "name": "black_background",
+                    "path": "assets/images/black_background.png"
+                },
+                {
+                    "name": "outsidetheboxlogo",
+                    "path": "assets/images/outsidetheboxlogo.png"
+                },
+                {
+                    "name": "debug",
+                    "path": "assets/images/debug.png"
+                },
+                {
+                    "name": "dragon_attack",
+                    "path": "assets/images/dragon_attack.png"
+                },
+                {
+                    "name": "dragon_die",
+                    "path": "assets/images/dragon_die.png"
+                },
+                {
+                    "name": "dragon_fireball",
+                    "path": "assets/images/dragon_fireball.png"
+                },
+                {
+                    "name": "dragon_fireball_impact",
+                    "path": "assets/images/dragon_fireball_impact.png"
+                },
+                {
+                    "name": "dragon_fly",
+                    "path": "assets/images/dragon_fly.png"
+                },
+                {
+                    "name": "dragon_hit",
+                    "path": "assets/images/dragon_hit.png"
+                },
+                {
+                    "name": "fireball",
+                    "path": "assets/images/fireball.png"
+                },
+                {
+                    "name": "fireball_impact",
+                    "path": "assets/images/fireball_impact.png"
+                },
+                {
+                    "name": "flag",
+                    "path": "assets/images/flag.png"
+                },
+                {
+                    "name": "knight_jump",
+                    "path": "assets/images/knight_jump.png"
+                },
+                {
+                    "name": "knight_walk",
+                    "path": "assets/images/knight_walk.png"
+                },
+                {
+                    "name": "peasent_attack",
+                    "path": "assets/images/peasent_attack.png"
+                },
+                {
+                    "name": "peasent_die",
+                    "path": "assets/images/peasent_die.png"
+                },
+                {
+                    "name": "peasent_hit",
+                    "path": "assets/images/peasent_hit.png"
+                },
+                {
+                    "name": "peasent_idle",
+                    "path": "assets/images/peasent_idle.png"
+                },
+                {
+                    "name": "peasent_jump",
+                    "path": "assets/images/peasent_jump.png"
+                },
+                {
+                    "name": "peasent_walk",
+                    "path": "assets/images/peasent_walk.png"
+                },
+                {
+                    "name": "princess_walk",
+                    "path": "assets/images/princess_walk.png"
+                },
+                {
+                    "name": "village_sequence",
+                    "path": "assets/images/village_sequence.png"
+                },
+                {
+                    "name": "wizard_2_attack_e",
+                    "path": "assets/images/wizard_2_attack_e.png"
+                },
+                {
+                    "name": "wizard_2_die1",
+                    "path": "assets/images/wizard_2_die1.png"
+                },
+                {
+                    "name": "wizard_2_die2",
+                    "path": "assets/images/wizard_2_die2.png"
+                },
+                {
+                    "name": "wizard_2_run",
+                    "path": "assets/images/wizard_2_run.png"
+                },
+                {
+                    "name": "wizard_attack_vfx_e_impact",
+                    "path": "assets/images/wizard_attack_vfx_e_impact.png"
+                },
+                {
+                    "name": "wizard_attack_vfx_e_shooting",
+                    "path": "assets/images/wizard_attack_vfx_e_shooting.png"
+                },
+                {
+                    "name": "wolf_attack",
+                    "path": "assets/images/wolf_attack.png"
+                },
+                {
+                    "name": "wolf_die",
+                    "path": "assets/images/wolf_die.png"
+                },
+                {
+                    "name": "wolf_hit",
+                    "path": "assets/images/wolf_hit.png"
+                },
+                {
+                    "name": "wolf_walk",
+                    "path": "assets/images/wolf_walk.png"
+                },
+                {
+                    "name": "zone1_forest",
+                    "path": "assets/images/zone1_forest.png"
+                },
+                {
+                    "name": "zone2_forest_sequence",
+                    "path": "assets/images/zone2_forest_sequence.png"
+                },
+                {
+                    "name": "zone4_mountain_sequence",
+                    "path": "assets/images/zone4_mountain_sequence.png"
+                }
+            ],
+            "sounds": [
+                {
+                    "name": "bosshit",
+                    "path": "assets/sounds/bosshit.wav"
+                },
+                {
+                    "name": "buttonselect",
+                    "path": "assets/sounds/buttonselect.wav"
+                },
+                {
+                    "name": "credits",
+                    "path": "assets/sounds/credits.mp3"
+                },
+                {
+                    "name": "death",
+                    "path": "assets/sounds/death.wav"
+                },
+                {
+                    "name": "deathmusic",
+                    "path": "assets/sounds/deathmusic.mp3"
+                },
+                {
+                    "name": "dragonbosstheme",
+                    "path": "assets/sounds/dragonbosstheme.mp3"
+                },
+                {
+                    "name": "dragonshout",
+                    "path": "assets/sounds/dragonshout.wav"
+                },
+                {
+                    "name": "enemyhit",
+                    "path": "assets/sounds/enemyhit.wav"
+                },
+                {
+                    "name": "evilcastle",
+                    "path": "assets/sounds/evilcastle.mp3"
+                },
+                {
+                    "name": "fire",
+                    "path": "assets/sounds/fire.wav"
+                },
+                {
+                    "name": "howtoplay",
+                    "path": "assets/sounds/howtoplay.mp3"
+                },
+                {
+                    "name": "maintheme",
+                    "path": "assets/sounds/maintheme.mp3"
+                },
+                {
+                    "name": "playerattack",
+                    "path": "assets/sounds/playerattack.wav"
+                },
+                {
+                    "name": "playerhit",
+                    "path": "assets/sounds/playerhit.wav"
+                },
+                {
+                    "name": "playerjump",
+                    "path": "assets/sounds/playerjump.wav"
+                },
+                {
+                    "name": "princessmusic",
+                    "path": "assets/sounds/princessmusic.mp3"
+                },
+                {
+                    "name": "win",
+                    "path": "assets/sounds/win.mp3"
+                },
+                {
+                    "name": "wizardbosstheme",
+                    "path": "assets/sounds/wizardbosstheme.mp3"
+                },
+                {
+                    "name": "wizardshot",
+                    "path": "assets/sounds/wizardshot.wav"
+                },
+                {
+                    "name": "zone1",
+                    "path": "assets/sounds/zone1.mp3"
+                },
+                {
+                    "name": "zone2",
+                    "path": "assets/sounds/zone2.mp3"
+                },
+                {
+                    "name": "zone3",
+                    "path": "assets/sounds/zone3.mp3"
+                },
+                {
+                    "name": "zonecomplete",
+                    "path": "assets/sounds/zonecomplete.wav"
+                }
+            ],
+            "zones": [
+                {
+                    "path": "assets/zones/opening.sequence.json"
+                },
+                {
+                    "path": "assets/zones/titlescreen.json"
+                },
+                {
+                    "path": "assets/zones/begin.sequence.json"
+                },
+                {
+                    "path": "assets/zones/howtoplay.sequence.json"
+                },
+                {
+                    "path": "assets/zones/zone1_1.json"
+                },
+                {
+                    "path": "assets/zones/zone1_2.json"
+                },
+                {
+                    "path": "assets/zones/zone1_3.json"
+                },
+                {
+                    "path": "assets/zones/zone2_1.json"
+                },
+                {
+                    "path": "assets/zones/zone2_2.json"
+                },
+                {
+                    "path": "assets/zones/zone2_3.json"
+                },
+                {
+                    "path": "assets/zones/zone3_1.json"
+                },
+                {
+                    "path": "assets/zones/zone3_2.json"
+                },
+                {
+                    "path": "assets/zones/zone3_3.json"
+                },
+                {
+                    "path": "assets/zones/zone4.json"
+                },
+                {
+                    "path": "assets/zones/zone5_1.json"
+                },
+                {
+                    "path": "assets/zones/zone5_2.json"
+                },
+                {
+                    "path": "assets/zones/zone5_3.json"
+                },
+                {
+                    "path": "assets/zones/zone6.json"
+                }
+            ]
+        }
+    });
+};
